8e795c8bdb4095bddf1c954164ee78f769e033d5
Sanjay <114412909+SANJAYGudHrt@users.noreply.github.com>
2022-11-20 12:26:41 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -3,9 +3,18 @@ class student:
         self.name=name
         self.age=age
     def details(self):
+        print("Name:",self.name,"\n","Age",self.age)
+class student2:
+    def fun(self,name,age):
+        self.name=name
+        self.age=age
+    def details(self):
         print("Name:",self.name,"\n","Age:",self.age)
-class update(student):
-    def call(self):
-        self.details()
-obj=update("Sanjay",20)
-obj.call()
+class student3(student,student2):
+    def fun(self,name,age):
+        self.name=name
+        self.age=age
+    def details(self):
+        print("Name:",self.name,"\n","Age:",self.age)
+obj=student3("sanjay","20")
+obj.details()
