741d4e35e5b9fd8b89d77464fadc111ea8a3ca59
Sanjay <114412909+SANJAYGudHrt@users.noreply.github.com>
2022-11-20 12:27:31 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
-class student:
-    def __init__(self,name,age):
-        self.name=name
-        self.age=age
-    def details(self):
-        print("Name:",self.name,"\n","Age",self.age)
-class student2:
-    def fun(self,name,age):
-        self.name=name
-        self.age=age
-    def details(self):
-        print("Name:",self.name,"\n","Age:",self.age)
-class student3(student,student2):
-    def fun(self,name,age):
-        self.name=name
-        self.age=age
-    def details(self):
-        print("Name:",self.name,"\n","Age:",self.age)
-obj=student3("sanjay","20")
-obj.details()
+English1=int(input("enter english mark:"))
+tamil1=int(input("enter tamil mark:"))
+maths1=int(input("enter maths mark:"))
+a1=English1+tamil1+maths1
+print("total marks:",a1)
+b1=a1/3
+print("average mark:",b1)
+English2=int(input("enter english mark:"))
+tamil2=int(input("enter tamil mark:"))
+maths2=int(input("enter maths mark:"))
+a2=English2+tamil2+maths2
+print("total marks:",a2)
+b2=a2/3
+print("average mark:",b2)
+English3=int(input("enter english mark:"))
+tamil3=int(input("enter tamil mark:"))
+maths3=int(input("enter maths mark:"))
+a3=English3+tamil3+maths3
+print("total marks:",a3)
+b3=a3/3
+print("average mark:",b3)
